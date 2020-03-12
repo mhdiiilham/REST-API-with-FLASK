48362dc1d72da12bb84feaa747bcef0b613dde59
mhdiiilham <muhd.iiilham@gmail.com>
2020-03-12 18:56:34 +0700

    GET response

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+STUDENTS = {
+  '1': {'name': 'Mark', 'age': 23, 'spec': 'math'},
+  '2': {'name': 'Jane', 'age': 20, 'spec': 'biology'},
+  '3': {'name': 'Peter', 'age': 21, 'spec': 'history'},
+  '4': {'name': 'Kate', 'age': 22, 'spec': 'science'},
+}
 
-@app.route('/')
+@app.route('/students')
 def hello():
-  return {'hello': 'world'}
-
+  return STUDENTS
 
 app.run()
